010f10730a5f854102a862c4c5d72305cb8215ab
Nishchitha A <nischiashok0@gmail.com>
2025-09-02 13:32:52 +0530

    project

--- Module/vm/main.tf
+++ Module/vm/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine" "vm" {
   name                          = var.name
   location                      = var.rg_location
   resource_group_name           = var.rg_name
-  network_interface_ids = [azurerm_network_interface.privateip.id]
+  network_interface_ids         = [azurerm_network_interface.privateip.id]
   vm_size                       = "Standard_B2s"
   delete_os_disk_on_termination = true
 
