aca9a44e7636c087c8b969c1ad04af77861566e5
Nishchitha A <nischiashok0@gmail.com>
2025-09-06 16:47:31 +0530

    project

--- Module/vm/data.tf
+++ Module/vm/data.tf
@@ -1,6 +1,6 @@
 data "vault_generic_secret" "ssh" {
-  path = "roboshop-infra/ssh"
+  path = "roboshop-infra/data/ssh"
 }
 data "vault_generic_secret" "ssh" {
-  path = "roboshop-dev/ssh"
+  path = "roboshop-dev/data/ssh"
 }
