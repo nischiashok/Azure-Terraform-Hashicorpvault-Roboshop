85f9e01f20d3ce6daf7eb2df1d3fbd6870d460a0
Nishchitha A <nischiashok0@gmail.com>
2025-09-06 16:52:28 +0530

    changes in vault secret name

--- Module/vm/data.tf
+++ Module/vm/data.tf
@@ -1,6 +1,3 @@
-data "vault_generic_secret" "ssh" {
-  path = "roboshop-infra/data/ssh"
-}
-data "vault_generic_secret" "ssh" {
-  path = "roboshop-dev/data/ssh"
+data "vault_generic_secret" "sec" {
+  path = "roboshop-infra/data/sec"
 }
