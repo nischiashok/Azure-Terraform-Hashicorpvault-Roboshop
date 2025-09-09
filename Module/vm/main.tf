7d85fa719ae111b1666f7c07596fdf1530f7409b
Nishchitha A <nischiashok0@gmail.com>
2025-09-09 21:47:36 +0530

    for testing from gpt

--- Module/vm/main.tf
+++ Module/vm/main.tf
@@ -63,20 +63,12 @@ resource "null_resource" "ansible" {
     password = data.vault_generic_secret.ssh.data["password"]
     host     = azurerm_network_interface.privateip.private_ip_address
   }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo dnf -y update || true",
-      "sudo dnf -y install python3 python3-pip || sudo apt-get update && sudo apt-get -y install python3 python3-pip",
-      "sudo python3 -m pip install --upgrade pip",
-      "sudo python3 -m pip install ansible hvac",
-        # run ansible-pull under bash to avoid PATH issues
-      "sudo bash -lc 'ansible-pull -i localhost, -U https://github.com/nischiashok/Azure-Roboshop-Terraform-ansible.git roboshop.yml -e app_name=${var.name} -e env=dev -e token=${var.token}'"
-
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo dnf install python3 python3 -pip -y",
-#       "sudo pip3 install ansible hvac",
-#       "ansible-pull -i localhost, -U https://github.com/nischiashok/Azure-Roboshop-Terraform-ansible.git roboshop.yml -e app_name=${var.name} -e env=dev -e token=${var.token}"
+      "sudo dnf install python3 python3 -pip -y",
+      "sudo pip3 install ansible hvac",
+      "ansible-pull -i localhost, -U https://github.com/nischiashok/Azure-Roboshop-Terraform-ansible.git roboshop.yml -e app_name=${var.name} -e env=dev -e token=${var.token}"
 
     ]
   }
